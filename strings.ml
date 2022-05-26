99ce1d36933a22a4a53e931ed46de6bb88a49761
Matcha-Teaa <danielmatcha33@gmail.com>
2022-05-26 15:38:49 -0400

    add func fstlet

--- strings.ml
+++ strings.ml
@@ -6,3 +6,12 @@ let getLength str1 = String.length str1
 (* Concat two strings*)
 let cc str1 str2 = str1 ^ str2
 
+(* Check if a string contains a certain letter *)
+(* Ex: contains "Hello" 'H' -> true *)
+let contains str letter = String.contains str letter
+
+(* Gets the first letter in a string *)
+let fstlet str = 
+    if String.length str != 0 then str.[0] else failwith "no letters in string"
+
+
